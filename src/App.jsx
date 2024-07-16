57ca0c89bd3753ad5e78fe9994b769046f4e4ac9
OctavioCecio <octaviocecio@gmail.com>
2024-07-16 20:34:54 -0300

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Box } from "@mui/material";
 import "./App.css";
+import { Footer } from "./components/footer";
 import SearchAppBar from "./components/navbar";
-
 function App() {
   return (
     <>
       <SearchAppBar />
+      <Box height={"100vw"}> 
+        
+      </Box>
+      <Footer></Footer>
     </>
   );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+export  function Footer() {
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <Box width={"100vw"} position="absolute" pb={"0px"} >
+    <BottomNavigation
+      showLabels
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+    >
+      <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+      <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+    </BottomNavigation>
+  </Box>
+  );
+}
