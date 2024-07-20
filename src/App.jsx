dbf27dc243d7a24847b6858a8336fbabc5ca53f6
OctavioCecio <octaviocecio@gmail.com>
2024-07-20 19:34:26 -0300

    contacto

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@ import SearchAppBar from "./components/navbar";
 import SimpleContainer from "./components/presentacion";
 import FotosDeComida from "./components/localYMenu";
 import Ubicacion from "./components/mapa";
+import { Horario } from "./components/contacto";
+
 function App() {
   return (
     <>
       <SearchAppBar />
       <SimpleContainer></SimpleContainer>
       <FotosDeComida></FotosDeComida>
+      <Horario></Horario>
       <Ubicacion></Ubicacion>
+      
       <Footer></Footer>
     </>
   );
