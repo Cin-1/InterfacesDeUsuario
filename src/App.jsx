4f4ab275924ebbc26ed49ce779f9cba7239130c3
OctavioCecio <octaviocecio@gmail.com>
2024-07-20 01:01:00 -0300

    Ubicacion + link en imagen

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import { Footer } from "./components/footer";
 import SearchAppBar from "./components/navbar";
 import SimpleContainer from "./components/presentacion";
 import FotosDeComida from "./components/localYMenu";
+import Ubicacion from "./components/mapa";
 function App() {
   return (
     <>
       <SearchAppBar />
       <SimpleContainer></SimpleContainer>
       <FotosDeComida></FotosDeComida>
-
+      <Ubicacion></Ubicacion>
       <Footer></Footer>
     </>
   );
